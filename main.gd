367435cee62750e78bb7b6f3725c9d4e567a6d72
edy_piojo <edytzintzun4@gmail.com>
2024-05-27 09:56:39 -0700

    mob

--- main.gd
+++ main.gd
@@ -8,6 +8,9 @@ func _ready():
 	screen_size = get_window().size
 	PlayerInitialPoosition = $Player.position
 
+func new_game():
+	$Floor.position.x = 0
+	$Player.position = PlayerInitialPoosition
 
 func _process(delta):
 	$Floor.position.x = $Player.position.x - 150
