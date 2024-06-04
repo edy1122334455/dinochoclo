ffc54d4185d9e7c5403752ef234407ee038c4ad7
edy_piojo <edytzintzun4@gmail.com>
2024-06-04 11:59:38 -0700

    acabe

--- main.gd
+++ main.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@export var mob_scene: PackedScene
 
 var screen_size : Vector2i
 
@@ -7,10 +8,28 @@ var PlayerInitialPoosition
 func _ready():
 	screen_size = get_window().size
 	PlayerInitialPoosition = $Player.position
-
+	GHUD.start_game.connect(new_game)
+	
+func game_over():
+	GHUD.update_high_score_label()
+	$MOBTimer.stop()
+	GHUD.mob_counter = 0
+	
 func new_game():
+	print('empieza')
 	$Floor.position.x = 0
 	$Player.position = PlayerInitialPoosition
+	$MOBTimer.start()
 
 func _process(delta):
 	$Floor.position.x = $Player.position.x - 150
+
+func _on_mob_timer_timeout():
+	print(GHUD.mob_counter)
+	if GHUD.mob_counter < 2:
+		var mob = mob_scene.instantiate()
+		mob.position.x = $Player.position.x + 1500
+		mob.position.y = 550
+		add_child(mob)
+		mob.hit.connect(game_over)
+		$MOBTimer.wait_time = 2
--- mob/mob.gd
+++ mob/mob.gd
@@ -4,23 +4,29 @@ signal hit
 
 
 func _ready():
+	GHUD.mob_counter = GHUD.mob_counter + 1
 	var collisions = [
 		$CollisionA,
-
+		$ColFlor,
+		$ColBala
 	]
 	var sprites = [
 		$SpriteA,
-
+		$SpriteFlor,
+		$SpriteBala
 	]
 	var random_int = randi() % collisions.size()
 	var selected_collisions = collisions [random_int]
 	var selected_sprite = sprites[random_int]
 	selected_collisions.disabled = false
 	selected_sprite.visible = true
+	selected_collisions.visible = true
 	
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	if GHUD.mob_counter > 0:
+		GHUD.mob_counter = GHUD.mob_counter - 1
+	queue_free()    
 
 
 func _on_body_entered(body):
